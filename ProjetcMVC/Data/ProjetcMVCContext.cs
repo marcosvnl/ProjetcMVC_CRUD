6bd37b85a755144afdd269281536ce7211526888
JIDD Sistemas - Dev <JIDD Sistemas - Dev@DESKTOP-AI7K7N2>
2021-04-03 12:47:16 -0300

    CRUD Scaffolding

---
+++ ProjetcMVC/Data/ProjetcMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetcMVC.Models;
+
+namespace ProjetcMVC.Data
+{
+    public class ProjetcMVCContext : DbContext
+    {
+        public ProjetcMVCContext (DbContextOptions<ProjetcMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetcMVC.Models.Department> Department { get; set; }
+    }
+}
