cee2f00333a95054fb92ab32f63e06643533d390
Marcos Vinicius <maarcosvnl@gmail.com>
2021-04-08 18:23:52 -0300

    Update seller and custom service exception

--- ProjetcMVC/Controllers/SellersController.cs
+++ ProjetcMVC/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using ProjetcMVC.Models.ViewModels;
 using ProjetcMVC.Services;
 using System;
 using System.Collections.Generic;
+using ProjetcMVC.Services.Exceptions;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -79,6 +80,53 @@ namespace ProjetcMVC.Controllers
             }
 
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                    return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel
+            {
+                Seller = obj,
+                Departments = departments
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
--- ProjetcMVC/Services/SellerService.cs
+++ ProjetcMVC/Services/SellerService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ProjetcMVC.Data;
 using ProjetcMVC.Models;
+using ProjetcMVC.Services.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -41,6 +42,24 @@ namespace ProjetcMVC.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
