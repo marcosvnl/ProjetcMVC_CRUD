c9a82265e9a0a5712d73467ae5be541629ae1b13
Marcos Vinicius <maarcosvnl@gmail.com>
2021-04-09 10:44:25 -0300

    App locale, number and date formatting

--- ProjetcMVC/Models/Seller.cs
+++ ProjetcMVC/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace ProjetcMVC.Models
@@ -8,9 +9,19 @@ namespace ProjetcMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        
+        [Display(Name = "Bird Date")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+        
+        [Display(Name = "Base Salary")]
+        [DataType(DataType.Currency)] 
+        //ou [DisplayFormat(DataFormatString = "{0:F2}")] => 2 casas decimal       
         public double BaseSlary { get; set; }
+        
         public Department Department { get; set; }
         //O EF Core vai entender q precisados de uma chave estrangeira para um vendedor resceber o Id de um departamento
         public int DepartmentId { get; set; } 
--- ProjetcMVC/Startup.cs
+++ ProjetcMVC/Startup.cs
@@ -1,17 +1,15 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using ProjetcMVC.Data;
 using ProjetcMVC.Services;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace ProjetcMVC
 {
@@ -50,6 +48,17 @@ namespace ProjetcMVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline. E add a dependência SeedingService para popular o db
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            //difinir a localidade da aplicação. Nesse exemplo será a localização do EUA
+            //var enUS = new CultureInfo("en-US");
+            //var localizationOptions = new RequestLocalizationOptions
+            //{
+            //    DefaultRequestCulture = new RequestCulture(enUS),
+            //    SupportedCultures = new List<CultureInfo> { enUS },
+            //    SupportedUICultures = new List<CultureInfo> { enUS }
+            //};
+
+            //app.UseRequestLocalization(localizationOptions);
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
